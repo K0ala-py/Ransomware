6aa1de6d5365030d4dca62add82c3a736c77e988
K0ala <92472219+K0ala-py@users.noreply.github.com>
2023-01-02 12:52:13 +0100

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,5 +1,5 @@
 from cryptography.fernet import Fernet
-def dec_process(file,key):
+def dec_process(file,key):                      # this is an funcion used in "ransomware.py" file for encrypt
     f = Fernet(key)
     with open(file,'rb') as encrypted_file:
         encrypted = encrypted_file.read()
