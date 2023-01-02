c491aa57a151564e175ce6751a4cdc1b9bbe0755
K0ala <92472219+K0ala-py@users.noreply.github.com>
2023-01-02 12:50:49 +0100

    Update decrypt_ransomware.py

--- decrypt_ransomware.py
+++ decrypt_ransomware.py
@@ -3,22 +3,22 @@ keys_book = open('keys.txt','r')
 
 listachiavi = []
 listafile = []
-for line in keys_book:
-    line = line.replace('\n', '')
-    if 'FILE:' in line:
-        listafile.append(line[6:]+'.lock')
-    if 'KEYPASSWORD:' in line:
+for line in keys_book:                              # for line in key file
+    line = line.replace('\n', '')   
+    if 'FILE:' in line:                             # if the "FILE" work is in line 
+        listafile.append(line[6:]+'.lock')          # add file name into the "listafile" list
+    if 'KEYPASSWORD:' in line:                      # idem for "KEYPASSWORD" word
         listachiavi.append(line[13:])
 
-for i in range(len(listafile)):
-    from cryptography.fernet import Fernet
+for i in range(len(listafile)):                     # for any element into "listafile" = any encrypt file
+    from cryptography.fernet import Fernet          # import fernet library
     f = Fernet(listachiavi[i])
-    with open(listafile[i],'rb') as encrypted_file:
-        encrypted = encrypted_file.read()
-        decrypted = f.decrypt(encrypted)
+    with open(listafile[i],'rb') as encrypted_file: 
+        encrypted = encrypted_file.read()           # read encrypt file and
+        decrypted = f.decrypt(encrypted)            # decrypt here
     with open(listafile[i],'wb') as decrypted_file:
         decrypted_file.write(decrypted)
-    os.rename(listafile[i],listafile[i][:-5])
+    os.rename(listafile[i],listafile[i][:-5])       # rename file from "example.txt.lock" in "example.txt"
 
 
-keys_book.close()
+keys_book.close()                                   # close key book file
